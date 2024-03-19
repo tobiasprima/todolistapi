ef8591f0862f051e8f461051bcebab3b79610e68
Tobias Prima <primatjahja@gmail.com>
2024-03-19 18:49:59 +0800

    feat: todos models

---
+++ internal/models/models.go
@@ -0,0 +1,14 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Todos struct {
+	ID		primitive.ObjectID	`json:"_id,omitempty" bson:"_id,omitempty"`
+	Title	string				`json:"title,omitempty" bson:"title"`
+	Status	bool				`json:"status,omitempty" bson:"status"`
+}
+
+type CreateTodosRequest struct{
+	Title	string				`json:"title,omitempty" bson:"title"`
+	Status	bool				`json:"status,omitempty" bson:"status"`
+}
