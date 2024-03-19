dacb1e7d3d4a0b50e17d4f1504a0809bb7b89994
Tobias Prima <88434271+tobiasprima@users.noreply.github.com>
2024-03-19 19:42:23 +0800

    feat: setup mongodb (#2)

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"os"
+	"todolist-api/internal/database"
+
+	"github.com/joho/godotenv"
 )
 
 func main(){
-	log.Println("test")
+
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file:", err)
+		return
+	}
+
+	databaseUri := os.Getenv("DATABASE_URI")
+
+	err = database.InitMongoDb((databaseUri), "todolist")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Connected to MongoDB")
+
+	defer func(){
+		err := database.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}()
 }
---
+++ internal/database/mongodb.go
@@ -0,0 +1,35 @@
+package database
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var(
+	client *mongo.Client
+	Todos *mongo.Collection
+)
+
+func InitMongoDb(uri string, database string) error {
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+
+	localClient, err := mongo.Connect(context.Background(), opts)
+	if err != nil {
+		return err
+	}
+
+	client = localClient
+
+	Todos = client.Database(database).Collection("todos")
+
+	err = client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err()
+	return err
+}
+
+func Close() error {
+	return client.Disconnect(context.Background())
+}
