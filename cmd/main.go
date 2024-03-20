ecec0a809aad304fb8fedabfe51dba26f0566333
Tobias Prima <primatjahja@gmail.com>
2024-03-20 23:37:22 +0800

    feat: delete for build

--- cmd/main.go
+++ cmd/main.go
@@ -8,20 +8,12 @@ import (
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 )
 
 func main(){
-
-	err := godotenv.Load()
-	if err != nil {
-		fmt.Println("Error loading .env file:", err)
-		return
-	}
-
 	databaseUri := os.Getenv("DATABASE_URI")
 
-	err = database.InitMongoDb((databaseUri), "todolist")
+	err := database.InitMongoDb((databaseUri), "todolist")
 
 	if err != nil {
 		fmt.Println(err)
